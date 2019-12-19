c5eb7d0faf31762d576a66157d7626939aab391a
Agabekov Roman <r.agabekov@initlab.ru>
2019-12-19 06:56:12 +0000

    Added base logic to the script

--- mysqlconfigurer.sh
+++ mysqlconfigurer.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-wget http://mysqltuner.pl/ -O mysqltuner.pl
+# Variables
+MYSQLCONFIGURER_PATH="/tmp/.mysqlconfigurer"
+MYSQLTUNER_FILENAME=$MYSQLCONFIGURER_PATH'/mysqltuner.pl'
+MYSQLTUNER_REPORT=$MYSQLCONFIGURER_PATH'/mysqltunerreport.json'
+MYSQLCONFIGURER_CONFIGFILE=$MYSQLCONFIGURER_PATH'/mysql.conf'
 
-perl mysqltuner.pl --json --verbose --notbstat --outputfile='mysqltunerreport.json'
+command -v curl >/dev/null 2>&1 || { echo >&2 "Curl is not installed. Please install Curl. Aborting."; exit 1; }
+command -v perl >/dev/null 2>&1 || { echo >&2 "Perl is not installed. Please install Perl. Aborting."; exit 1; }
 
-curl -d "@/root/mysqltuner/1.json" -X POST https://z3sog3l1l7.execute-api.us-east-1.amazonaws.com/dev/mysqltunerreport
+# Check if the tmp folder exists
+if [ -d "$DIR" ]; then
+    # Clear tmp directory
+    rm $MYSQLCONFIGURER_PATH/*
+else
+    # Create tmp directory 
+    mkdir $MYSQLCONFIGURER_PATH
+fi
+
+# Download last version of the MySQLTuner
+curl -o $MYSQLTUNER_FILENAME -L http://mysqltuner.pl/
+
+# Run MySQLTuner for creating report in the JSON format
+perl $MYSQLTUNER_FILENAME --json --verbose --notbstat --outputfile="$MYSQLTUNER_REPORT"
+
+# Post MySQLTuner report in the AIOps service. The answer is the configuration file for MySQL
+curl -d \"@$MYSQLTUNER_REPORT\" -X POST https://api.servers-support.com/v1/mysql -o "$MYSQLCONFIGURER_CONFIGFILE"

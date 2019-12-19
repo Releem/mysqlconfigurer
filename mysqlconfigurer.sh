93046b8d2fe50d251b9bd99284f910de5e2a89e9
Agabekov Roman <r.agabekov@initlab.ru>
2019-12-19 08:58:01 +0000

    added header for curl fix tmp directory name change variables

--- mysqlconfigurer.sh
+++ mysqlconfigurer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Variables
-MYSQLCONFIGURER_PATH='/tmp/.mysqlconfigurer'
-MYSQLTUNER_FILENAME=$MYSQLCONFIGURER_PATH'/mysqltuner.pl'
-MYSQLTUNER_REPORT=$MYSQLCONFIGURER_PATH'/mysqltunerreport.json'
-MYSQLCONFIGURER_CONFIGFILE=$MYSQLCONFIGURER_PATH'/z_aiops_mysql.conf'
+MYSQLCONFIGURER_PATH="/tmp/.mysqlconfigurer/"
+MYSQLTUNER_FILENAME=$MYSQLCONFIGURER_PATH"mysqltuner.pl"
+MYSQLTUNER_REPORT=$MYSQLCONFIGURER_PATH"mysqltunerreport.json"
+MYSQLCONFIGURER_CONFIGFILE=$MYSQLCONFIGURER_PATH"z_aiops_mysql.conf"
 
 command -v curl >/dev/null 2>&1 || { echo >&2 "Curl is not installed. Please install Curl. Aborting."; exit 1; }
 command -v perl >/dev/null 2>&1 || { echo >&2 "Perl is not installed. Please install Perl. Aborting."; exit 1; }
 
 # Check if the tmp folder exists
-if [ -d "$DIR" ]; then
+if [ -d "$MYSQLCONFIGURER_PATH" ]; then
     # Clear tmp directory
     rm $MYSQLCONFIGURER_PATH/*
 else
@@ -25,4 +25,4 @@ curl -o $MYSQLTUNER_FILENAME -L http://mysqltuner.pl/
 perl $MYSQLTUNER_FILENAME --json --verbose --notbstat --outputfile="$MYSQLTUNER_REPORT"
 
 # Post MySQLTuner report in the AIOps service. The answer is the configuration file for MySQL
-curl -d \"@$MYSQLTUNER_REPORT\" -X POST https://api.servers-support.com/v1/mysql -o "$MYSQLCONFIGURER_CONFIGFILE"
+curl -d @$MYSQLTUNER_REPORT -H "Content-Type: application/json" -X POST https://api.servers-support.com/v1/mysql -o "$MYSQLCONFIGURER_CONFIGFILE"

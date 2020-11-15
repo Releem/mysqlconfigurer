fa0fa0b7e58892696a4d21ad5c8f6ebce1ffe82f
Agabekov Roman <r.agabekov@initlab.ru>
2020-11-15 07:57:54 +0000

    1. Download MySQLTuner if it doesn't exist 2. Added parameter Releem API Key 3. Added parameter MYSQL_MEMORY_LIMIT to set available memory for MYSQL

--- mysqlconfigurer.sh
+++ mysqlconfigurer.sh
@@ -5,6 +5,23 @@ MYSQLCONFIGURER_PATH="/tmp/.mysqlconfigurer/"
 MYSQLTUNER_FILENAME=$MYSQLCONFIGURER_PATH"mysqltuner.pl"
 MYSQLTUNER_REPORT=$MYSQLCONFIGURER_PATH"mysqltunerreport.json"
 MYSQLCONFIGURER_CONFIGFILE=$MYSQLCONFIGURER_PATH"z_aiops_mysql.cnf"
+setMYSQL_MEMORY_LIMIT=0
+
+# Parse parameters
+while getopts k:m option
+do
+case "${option}"
+in
+k) RELEEM_API_KEY=${OPTARG};;
+m) MYSQL_MEMORY_LIMIT=${OPTARG};;
+esac
+done
+
+# Check RELEEM_API_KEY is not empty
+if [ -z "$RELEEM_API_KEY" ]; then
+    echo >&2 "RELEEM_API_KEY is empty please sign up at https://app.releem.com/ to get your API key. Aborting."
+    exit 1;
+fi
 
 command -v curl >/dev/null 2>&1 || { echo >&2 "Curl is not installed. Please install Curl. Aborting."; exit 1; }
 command -v perl >/dev/null 2>&1 || { echo >&2 "Perl is not installed. Please install Perl. Aborting."; exit 1; }
@@ -19,20 +36,23 @@ else
     mkdir $MYSQLCONFIGURER_PATH
 fi
 
-# Download last version of the MySQLTuner
-curl -s -o $MYSQLTUNER_FILENAME -L http://mysqltuner.pl/ 
+# Check if MySQLTuner already downloaded and download if it doesn't exist
+if [ ! -f "$MYSQLTUNER_FILENAME" ]; then
+    # Download latest version of the MySQLTuner
+    curl -s -o $MYSQLTUNER_FILENAME -L http://mysqltuner.pl/ 
+fi
 
 # Run MySQLTuner for creating report in the JSON format
-if perl $MYSQLTUNER_FILENAME --json --verbose --notbstat --outputfile="$MYSQLTUNER_REPORT" --defaults-file ~/.my.cnf > /dev/null; then 
+if perl $MYSQLTUNER_FILENAME --json --verbose --notbstat --forcemem=$MYSQL_MEMORY_LIMIT --outputfile="$MYSQLTUNER_REPORT" --defaults-file ~/.my.cnf > /dev/null; then 
 
     # Post MySQLTuner report in the AIOps service. The answer is the configuration file for MySQL
-    curl -s -d @$MYSQLTUNER_REPORT -H "Content-Type: application/json" -X POST https://api.servers-support.com/v1/mysql -o "$MYSQLCONFIGURER_CONFIGFILE"
+    curl -s -d @$MYSQLTUNER_REPORT -H "x-releem-api-key: $RELEEM_API_KEY" -H "Content-Type: application/json" -X POST https://api.servers-support.com/v1/mysql -o "$MYSQLCONFIGURER_CONFIGFILE"
     exit
 else
 
     # If error then show report and exit
     errormsg="    \
-    \n\n\n\n--------MySQLTuner completed with error--------\n   \
+    \n\n\n\n--------Releem MySQLTuner completed with error--------\n   \
     \nCheck /tmp/.mysqlconfigurer/mysqltunerreport.json for details \n \
     \n--------Please fix the error and run again--------\n"
     printf "${errormsg}" >&2

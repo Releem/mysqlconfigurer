b24830f5aa759826e6257c790f412b55b617df7f
Dmitry Kochetov <d.kochetov@initlab.ru>
2023-11-07 18:22:51 +0300

    changed version for update

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 GOOS=linux GOARCH=amd64 go build -o releem-agent-x86_64
+GOOS=linux GOARCH=amd64 go build -o releem-agent-amd64
 GOOS=linux GOARCH=arm64 go build -o releem-agent-aarch64
+GOOS=freebsd GOARCH=amd64 go build -o releem-agent-freebsd-amd64

669301bec86c3b325718d49190abfa15c209f293
Dmitry <d.kochetov@initlab.ru>
2023-11-07 17:53:49 +0300

    Latency (#219)
    * changed collecting metrics for latency
    
    * Added calculation latency on agent
    
    * increased version

--- metrics/dbMetricsBase.go
+++ metrics/dbMetricsBase.go
@@ -2,6 +2,8 @@ package metrics
 
 import (
 	"database/sql"
+	"sort"
+	"strconv"
 
 	"github.com/Releem/mysqlconfigurer/config"
 	"github.com/advantageous/go-logback/logging"
@@ -69,18 +71,79 @@ func (DbMetricsBase *DbMetricsBaseGatherer) GetMetrics(metrics *Metrics) error {
 	}
 	// Latency
 	{
-		var row MetricValue
-		err := DbMetricsBase.db.QueryRow("select `s2`.`avg_us` AS `avg_us` from ((select count(0) AS `cnt`,round(`performance_schema`.`events_statements_summary_by_digest`.`AVG_TIMER_WAIT` / 1000000,0) AS `avg_us` from `performance_schema`.`events_statements_summary_by_digest` group by round(`performance_schema`.`events_statements_summary_by_digest`.`AVG_TIMER_WAIT` / 1000000,0)) `s1` join (select count(0) AS `cnt`,round(`performance_schema`.`events_statements_summary_by_digest`.`AVG_TIMER_WAIT` / 1000000,0) AS `avg_us` from `performance_schema`.`events_statements_summary_by_digest` group by round(`performance_schema`.`events_statements_summary_by_digest`.`AVG_TIMER_WAIT` / 1000000,0)) `s2` on(`s1`.`avg_us` <= `s2`.`avg_us`)) group by `s2`.`avg_us` having ifnull(sum(`s1`.`cnt`) / nullif((select count(0) from `performance_schema`.`events_statements_summary_by_digest`),0),0) > 0.95 order by ifnull(sum(`s1`.`cnt`) / nullif((select count(0) from `performance_schema`.`events_statements_summary_by_digest`),0),0) limit 1").Scan(&row.value)
+		var output []MetricGroupValue
+		var digest string
+		var calls, avg_time_us int
+
+		rows, err := DbMetricsBase.db.Query("SELECT CONCAT(IFNULL(schema_name, 'NULL'), '_', IFNULL(digest, 'NULL')) as queryid, count_star as calls, round(avg_timer_wait/1000000, 0) as avg_time_us FROM performance_schema.events_statements_summary_by_digest")
 		if err != nil {
 			if err != sql.ErrNoRows {
 				DbMetricsBase.logger.Error(err)
 			}
 		} else {
-			metrics.DB.Metrics.Latency = row.value
+			for rows.Next() {
+				err := rows.Scan(&digest, &calls, &avg_time_us)
+				if err != nil {
+					DbMetricsBase.logger.Error(err)
+					return err
+				}
+				digest := MetricGroupValue{"queryid": digest, "calls": calls, "avg_time_us": avg_time_us}
+				output = append(output, digest)
+			}
+		}
+		if len(output) != 0 {
+			totalQueryCount := len(output)
+			dictQueryCount := make(map[int]int)
+			listAvgTimeDistinct := make([]int, 0)
+			listAvgTime := make([]int, 0)
+
+			for _, query := range output {
+				avgTime := query["avg_time_us"].(int)
+				if !contains(listAvgTimeDistinct, avgTime) {
+					listAvgTimeDistinct = append(listAvgTimeDistinct, avgTime)
+				}
+				listAvgTime = append(listAvgTime, avgTime)
+			}
+			sort.Sort(sort.Reverse(sort.IntSlice(listAvgTimeDistinct)))
+
+			for _, avgTime1 := range listAvgTime {
+				for _, avgTime2 := range listAvgTimeDistinct {
+					if avgTime2 >= avgTime1 {
+						if _, ok := dictQueryCount[avgTime2]; !ok {
+							dictQueryCount[avgTime2] = 1
+						} else {
+							dictQueryCount[avgTime2]++
+						}
+					} else {
+						break
+					}
+				}
+			}
+
+			latency := 0
+			for _, avgTime := range listAvgTimeDistinct {
+				if float64(dictQueryCount[avgTime])/float64(totalQueryCount) <= 0.95 {
+					break
+				}
+				latency = avgTime
+			}
+
+			metrics.DB.Metrics.Latency = strconv.Itoa(latency)
+		} else {
+			metrics.DB.Metrics.Latency = ""
 		}
 	}
 
 	DbMetricsBase.logger.Debug("collectMetrics ", metrics.DB.Metrics)
 	return nil
 
+}
+
+func contains(arr []int, num int) bool {
+	for _, n := range arr {
+		if n == num {
+			return true
+		}
+	}
+	return false
 }

7c99a79b41e9f9d73b75e362470ca670989549a0
Dmitry Kochetov <d.kochetov@initlab.ru>
2024-02-20 22:38:07 +0300

    Added new arch

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-GOOS=linux GOARCH=amd64 go build -o releem-agent-x86_64
-GOOS=linux GOARCH=amd64 go build -o releem-agent-amd64
-GOOS=linux GOARCH=arm64 go build -o releem-agent-aarch64
+GOOS=linux   GOARCH=amd64 go build -o releem-agent-x86_64
+GOOS=linux   GOARCH=amd64 go build -o releem-agent-amd64
+GOOS=linux   GOARCH=arm64 go build -o releem-agent-aarch64
+GOOS=linux     GOARCH=386 go build -o releem-agent-i686
 GOOS=freebsd GOARCH=amd64 go build -o releem-agent-freebsd-amd64

ee2530f8869d4792f241b143231113bd6efc0c4c
Dmitry Kochetov <d.kochetov@initlab.ru>
2024-03-19 12:47:01 +0300

    fixed reading data from queries

--- metrics/dbCollectQueries.go
+++ metrics/dbCollectQueries.go
@@ -36,7 +36,7 @@ func (DbCollectQueries *DbCollectQueries) GetMetrics(metrics *Metrics) error {
 	{
 		var output []MetricGroupValue
 		var schema_name, query string
-		var calls, avg_time_us int
+		var calls, avg_time_us, sum_time_us int
 
 		rows, err := DbCollectQueries.db.Query("SELECT IFNULL(schema_name, 'NULL') as schema_name, IFNULL(digest_text, 'NULL') as query, count_star as calls, round(avg_timer_wait/1000000, 0) as avg_time_us, round(SUM_TIMER_WAIT/1000000, 0) as sum_time_us FROM performance_schema.events_statements_summary_by_digest")
 		if err != nil {
@@ -45,12 +45,12 @@ func (DbCollectQueries *DbCollectQueries) GetMetrics(metrics *Metrics) error {
 			}
 		} else {
 			for rows.Next() {
-				err := rows.Scan(&schema_name, &query, &calls, &avg_time_us)
+				err := rows.Scan(&schema_name, &query, &calls, &avg_time_us, &sum_time_us)
 				if err != nil {
 					DbCollectQueries.logger.Error(err)
 					return err
 				}
-				digest := MetricGroupValue{"schema_name": schema_name, "query": query, "calls": calls, "avg_time_us": avg_time_us}
+				digest := MetricGroupValue{"schema_name": schema_name, "query": query, "calls": calls, "avg_time_us": avg_time_us, "sum_time_us": sum_time_us}
 				output = append(output, digest)
 			}
 		}

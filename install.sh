14b6fe857e0932be740c8c761afc718071f79c1a
Dmitry Kochetov <d.kochetov@initlab.ru>
2025-11-25 18:36:30 +0300

    Increased version

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# install.sh - Version 1.22.1
+# install.sh - Version 1.22.2
 # (C) Releem, Inc 2022
 # All rights reserved
 
@@ -9,7 +9,7 @@ export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/
 # using the package manager.
 
 set -e -E
-install_script_version=1.22.1
+install_script_version=1.22.2
 logfile="/var/log/releem-install.log"
 
 WORKDIR="/opt/releem"
